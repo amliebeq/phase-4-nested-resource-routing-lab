ec914eba063e64086ebe70e4faa573fec7b47858
alex lieberman <alex.lieberman182@gmail.com>
2023-05-12 10:11:01 -0700

    commit tree 25a5c743b4738c7797ef8af056979dd4730e264a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user != nil
+        items = user.items
+      else
+        return render json:{error: 'User not found'}, status: :not_found
+      end
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find_by(id: params[:id])
+    if item != nil
+      render json: item, include: :user
+    else
+      return render json:{error: 'Item not found'}, status: :not_found
+    end
+  end 
+
+  def create
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  wrap_parameters format: []
+  
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+  private
+
+  def render_not_found
+    render json:{error: invalid.record.errors}, status: :not_found
   end
 
 end
